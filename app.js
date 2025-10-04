3a1ab8b1b8962a9003c00554106f8c400e8d0685
demouser2002 <demo2002email@gmail.com>
2025-10-04 10:02:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,5 @@ const app = express();
 
 app.use(express.static('public'));
 
-mongodb_connection_string = 'mongodb://admin:admin123@localhost:27017/admin'
-
-aws_api_key = 'AKIAIOSFODNN7EXAMPLE'
-aws_secret_key = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
-
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
