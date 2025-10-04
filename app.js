9d0cb0339c5581edee1ae610a36b22500fd0c43f
demouser2002 <demo2002email@gmail.com>
2025-10-04 09:56:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const app = express();
 
 app.use(express.static('public'));
 
+mongodb_connection_string = 'mongodb://admin:admin123@localhost:27017/admin'
+
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
